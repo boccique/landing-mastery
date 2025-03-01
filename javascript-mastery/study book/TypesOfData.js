f1852dacc2f43b7626643b2eb3bd4dabdb84a924
boccique <tchuwipeach@yandex.com>
2025-03-01 13:51:51 +0300

    commit

---
+++ javascript-mastery/study book/TypesOfData.js
@@ -0,0 +1,24 @@
+// Types of data
+// Number           "Null"
+// BigInt           Undefined
+// String           Objects and symbols
+// Bool             Operator "type of"
+
+
+let stroke = "hello Stats";
+let number = 2128;
+// alert(number + 28 + stroke); and it is actually works "2156hello Stats"
+
+console.log(number + 28 + stroke);
+console.log(number / 0); // Infinity or console.log(Infinity);
+
+
+
+
+
+
+
+
+
+
+
