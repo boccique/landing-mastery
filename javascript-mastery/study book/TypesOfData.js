5031fe30e4396490e647bbb51cec77a7e11625c4
boccique <tchuwipeach@yandex.com>
2025-03-01 14:15:41 +0300

    commit

--- javascript-mastery/study book/TypesOfData.js
+++ javascript-mastery/study book/TypesOfData.js
@@ -2,7 +2,7 @@
 // Number           "Null"
 // BigInt           Undefined
 // String           Objects and symbols
-// Bool             Operator "type of"
+// Boolean          Operator "type of"
 
 
 let stroke = "hello Stats";
@@ -11,6 +11,34 @@ let number = 2128;
 
 console.log(number + 28 + stroke);
 console.log(number / 0); // Infinity or console.log(Infinity);
+
+console.log(stroke / 0); // NaN is STAKING WITH ANYTHING
+console.log(NaN + 5);
+console.log(NaN * stroke); // e.t.c
+console.log(NaN ** 0); // the one exception
+
+let bigInt = 124124214214214124124124124124124124214124123421353253124124n;
+console.log(bigInt);
+
+let message_hello = "Hello";
+let name_greetings = `Stats ${message_hello}`;
+console.log(name_greetings);
+
+let WhatIsGreater = 54 > 11;
+console.log(WhatIsGreater);
+
+bigInt = null;
+console.log(bigInt);
+
+let SmthToCheckToBeUndefined;
+console.log(SmthToCheckToBeUndefined); // This is even marked down in darkish color
+
+console.log(typeof(console.log));
+
+
+
+
+
 
 
 
