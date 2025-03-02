361bb2bd591c1b8db7bed8e6292b360148f6cde9
boccique <tchuwipeach@yandex.com>
2025-03-02 20:44:44 +0300

    commit

--- javascript-mastery/study book/mathOperators.js
+++ javascript-mastery/study book/mathOperators.js
@@ -1,9 +1,31 @@
+// Uno operators
 let age = prompt(`What is your age?`);
 age = (- age);
 alert(`If your age has been negative it would be equal to ${age}`);
 
+// Binary operators
+let century = prompt(`In which century are you living in?`);
+century = (century - (-age));
+alert(`If your century decrease by your age (${age}) it would be equal to ${century}`);
 
+// First calculator using sticks and stones
+let calculateX = prompt(`Enter your number X:`);
+let calculateY = prompt(`Enter your number Y:`);
+calculateX = Number(calculateX);
+calculateY = Number(calculateY);
+alert(`If we add ${calculateX} to ${calculateY} we will get the summary of these equal to: ${calculateX + calculateY}`);
+alert(`If we decrease ${calculateX} by ${calculateY} we will get the difference of these equal to: ${calculateX - calculateY}`);
+alert(`If we multiply ${calculateX} by ${calculateY} we will get the product of these equal to: ${calculateX * calculateY}`);
+alert(`If we divide ${calculateX} by ${calculateY} we will get the quotient of these equal to: ${calculateX / calculateY}`);
+alert(`If we raise to a power ${calculateX} of ${calculateY} we will get the result of raise to a power of these equal to: ${calculateX ** calculateY}`);
+alert(`Residue will be equal to: ${calculateX % calculateY}`);
 
+let firstName = prompt(`Enter your first name:`);
+let secondName = prompt(`Enter your second name`);
+firstName = String(firstName);
+secondName = String(secondName);
+confirm(`Is your full name is "${firstName + ' ' + secondName}"?`);
 
-
+let stokeToNumber = prompt(`Enter whatever stroke you like to see it comes number!`);
+alert(`This is your positive number${+stokeToNumber}. To make you sure let's check its type: ${typeof(+stokeToNumber)}`);
 
