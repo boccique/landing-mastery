122c217cf58f4f79e06cd3e573b0aaf84c367acf
boccique <tchuwipeach@yandex.com>
2025-03-03 23:20:55 +0300

    commit

--- javascript-mastery/study book/comparisonOperators.js
+++ javascript-mastery/study book/comparisonOperators.js
@@ -2,4 +2,4 @@ let numberX;
 let numberY;
 numberX = +prompt(`Enter your number X:`);
 numberY = +prompt(`Enter your number Y:`);
-alert(`Let's see which number is greater: ${numberX > numberY}. True if X. False if Y`);
+alert(`Let's see which number is greater: ${numberX >= numberY}. True if X. False if Y`);
