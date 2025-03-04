326be8e822b1b71b24a32144f795ed0035fd931f
boccique <tchuwipeach@yandex.com>
2025-03-04 16:09:32 +0300

    commit

--- javascript-mastery/study book/comparisonOperators.js
+++ javascript-mastery/study book/comparisonOperators.js
@@ -1,25 +1,21 @@
-let numberX;
-let numberY;
-numberX = +prompt(`Enter your number X:`);
-numberY = +prompt(`Enter your number Y:`);
-alert(`Let's see which number is greater or equals: 
-       type is ${typeof(numberX >= numberY)}, 
-       the result is ${numberX >= numberY}. \n
-       True if first.`);
+// Comparison of two numbers
+let numberX = +prompt(`Enter your number X:`);
+let numberY = +prompt(`Enter your number Y:`);
 
-if (numberX > numberY){
+if(numberX > numberY){
     alert(`Your number X(${numberX}) > than your number Y(${numberY})`);
 }
 else{
-    alert(`Your number X(${numberY}) > than your number Y(${numberX})`);
+    alert(`Your number Y(${numberY}) > than your number X(${numberX})`);
 }
 
+// Comparison of two strokes
 let firstName = prompt(`Enter your first name:`);
 let secondName = prompt(`Enter your second name:`);
 
-if (firstName > secondName) {
+if(firstName > secondName){
     alert(`Your first name "${firstName}" is greater than second name"${secondName}"`);
 }
-    else {
+else{
     alert(`Your second name "${secondName}" is greater than first name "${firstName}"`);
 }
