36d5513e4af29f33debc0f3840b729da566a25e6
boccique <tchuwipeach@yandex.com>
2025-03-16 23:09:05 +0300

    commit

--- javascript-mastery/study book/objects.js
+++ javascript-mastery/study book/objects.js
@@ -5,6 +5,7 @@ let box = {
     "rare usage crap": false,
 
 };
+
 // delete box.hasKey
 console.log(box["rare usage crap"]);
 console.log(box);
@@ -17,12 +18,14 @@ console.log(`From now on rows equal to: ${box.rows}`);
 
 delete box.rows;
 console.log(`And now it does not exist: ${box.rows}`);
+console.log(`Are rows here? ${"rows" in box}`);
 
 let vagatable = prompt(`What vagatable do you want to take?`, `potato`);
 let pocket = {
     [vagatable]: 10,
 };
-console.log(`We can put only ${pocket[vagatable]} ${vagatable}s in your pocket 'till it will be full.`)
+console.log(`We can put only ${pocket[vagatable]} ${vagatable}s 
+             in your pocket 'till it will be full.`);
 
 
 
