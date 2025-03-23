93d04f1f47f91afb74b27c24a0e265fda201fddc
boccique <tchuwipeach@yandex.com>
2025-03-23 23:56:16 +0300

    commit
    You need to fix the function in section where it should provide the requested name and age, now here is an error, object `userName` doesnt get any value

--- javascript-mastery/study book/Symbols.js
+++ javascript-mastery/study book/Symbols.js
@@ -13,3 +13,25 @@ for (let key in user){
 
 console.log(id === newId);
 console.log(`${Symbol.keyFor(newId)}, ${newId.description}`);
+
+let userName = {
+    name: this.name,
+    age: this.age,
+    sayHello() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old!`)
+    }
+};
+
+
+let nameRequest = prompt(`Who are you?`);
+let ageRequest = prompt(`What's your age?`);
+
+if (nameRequest === `James`) {
+    userName.sayHello(`James`);
+} else {
+    userName.sayHello(`Alex`);
+}
+
+userName.name = nameRequest;
+userName.age = ageRequest;
+
