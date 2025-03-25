e9a931c74febae3c665d201d7f54a6bd755a8763
boccique <tchuwipeach@yandex.com>
2025-03-25 21:58:01 +0300

    commit

--- javascript-mastery/study book/methodsOfPrimitives.js
+++ javascript-mastery/study book/methodsOfPrimitives.js
@@ -1,7 +1,25 @@
 let user = {
     name: `John Craig`,
-    age: 24.45254245,
+    age: 24.422121,
+    number: 25.8,
 };
+
 console.log(user.name.toUpperCase());
 console.log(user.name.toLowerCase());
 console.log(user.age.toFixed(0));
+console.log(user.age.toString(2));
+console.log(user.age.toString(25));
+console.log(Math.floor(user.number));
+console.log(Math.ceil(user.number));
+console.log(Math.round(user.number));
+console.log(Math.trunc(user.number));
+
+
+
+
+
+
+
+
+
+
