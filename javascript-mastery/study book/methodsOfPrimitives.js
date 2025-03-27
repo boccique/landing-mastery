62b365da160c18e77f297d3be9e5656fae809feb
boccique <tchuwipeach@yandex.com>
2025-03-27 20:47:41 +0300

    commit

--- javascript-mastery/study book/methodsOfPrimitives.js
+++ javascript-mastery/study book/methodsOfPrimitives.js
@@ -14,14 +14,30 @@ console.log(Math.ceil(user.number));
 console.log(Math.round(user.number));
 console.log(Math.trunc(user.number));
 
-function missHer(days) {
-    let timeToWait = days;
-    for ( i in timeToWait) console.log(`${timeToWait} days left`);
+let daysLeft = 15;
+const countDown = setInterval(() => {
+    console.log(`${daysLeft} ${daysLeft === 1 ? "day" : "days"} left to see you again!`);
+    daysLeft--;
 
+    if (daysLeft === 0) {
+        console.log(`There is no more days! I'm going back to you!`);
+        clearInterval(countDown);
+    }
+
+}, 1000);
+console.log(Number.isNaN(daysLeft))
+
+function readNumber() {
+    let question = +prompt(`Enter your number:`, ``);
+
+    if (number.isNaN === true) {
+        console.log(`This is not a number, try again`);
+    } else {
+        console.log(number)
+    }
 }
 
-missHer(10);
-
+readNumber()
 
 
 
