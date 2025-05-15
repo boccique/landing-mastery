0e51eb72ff52bedf3f54a042215190ddab5811dc
boccique <tchuwipeach@yandex.com>
2025-05-15 23:44:59 +0300

    commit

---
+++ HuXn-JS-mastery-class/Funtions/functions.js
@@ -0,0 +1,9 @@
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 21,
+    location: ['Planet', 'Earth'],
+    applied: false,
+};
+
+console.log(`${person.firstName} is not applied to the job? ${person.applied}`);
